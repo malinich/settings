373ed7fabcda9837d105d96fac7bd04be658b85e
igor.malinov <igor.malinov@w-MalinovII.demo.oim>
2017-12-11 12:22:03 +0300

    PY-2017.3 <igor.malinov@w-MalinovII Create IntelliLang.xml, project.default.xml, Default copy.xml, Django.xml, Default.xml, AngularJS .xml, applicationLibraries.xml, file.template.settings.xml, find.xml, bootstrap.xml, keymap.xml, code.style.schemes.xml, _@user_Darcula.icls, Angular2.xml, Zen HTML.xml, github_settings.xml, JavaScript File.js, flask.xml, Default.xml, JavaScript.xml, AngularJS.xml, Python Unit Test.py, duplocatorSettings.xml, Zen XSL.xml, Python.xml, editor.codeinsight.xml, editor.xml, uml.xml, debugger.xml, baseRefactoring.xml, ide.general.xml, git.xml, other.xml, ui.lnf.xml, COM.xml, filetypes.xml, Python Script.py, plugin_ui.xml, colors.scheme.xml, html_xml.xml, External Tools.xml, notifications.xml, Default copy 1.xml, databaseDrivers.xml, SQL.xml, customization.xml, Default copy 2.xml, hg.xml, laf.xml, Darcula Niyaz.icls, jdk.table.xml, path.macros.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, rSafe.xml, databaseSettings.xml, usageView.xml

---
+++ fileTemplates/internal/Python Unit Test.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+if __name__ == '__main__':
+    unittest.main()
