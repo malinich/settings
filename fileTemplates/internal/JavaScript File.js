72277c6a0e12c8a1e791ff1c189d96a6cadaef85
malinich <miltrix@yandex.ru>
2017-12-18 23:51:26 +0300

    PY-2017.3.1 <maka@maka-Pc Overwrite remote https://github.com/malinich/settings to local

--- fileTemplates/internal/JavaScript File.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-/**
- * __author__ = '$USER'
- */
--- fileTemplates/internal/Python Script.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-from __future__ import unicode_literals
-import logging
-
-__author__ = 'Niyaz.Batyrshin'
-logger = logging.getLogger(__name__)
